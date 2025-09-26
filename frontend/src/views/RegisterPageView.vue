c4e7177bc637982d597e3cc5d72b730b83467ac7
Aleksa <aleksamlsvc12@gmail.com>
2025-09-26 15:11:13 +0200

    Register page view created

---
+++ frontend/src/views/RegisterPageView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from "../components/Header.vue";
+import Main from "../components/Main.vue";
+import Footer from "../components/Footer.vue";
+import Register from "../components/Register.vue";
+</script>
+
+<template>
+  <Header />
+  <Main>
+    <Register />
+  </Main>
+  <Footer />
+</template>
