4e2de04d793fefd27d6b10f69cdbe69537cc384b
Aleksa <aleksamlsvc12@gmail.com>
2025-09-26 12:06:54 +0200

    Base layout components imported and tested

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,13 @@
-<template>
-    Hello World!
-</template>
+<script setup>
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+</script>
 
+<template>
+  <div class="flex flex-col h-screen w-full">
+    <Header/>
+    <Main/>
+    <Footer/>
+  </div>
+</template>
