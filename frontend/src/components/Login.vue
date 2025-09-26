8f4f9301932876868a38eed5b7e039d5b16126fc
Aleksa <aleksamlsvc12@gmail.com>
2025-09-26 14:51:06 +0200

    Login component created

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="h-full flex justify-center items-center font-mono">
+    <form class="flex flex-col justify-between w-1/4 h-[75%] p-10 bg-white">
+      <p class="text-lg font-bold text-center">Login</p>
+
+      <div>
+        <label for="Email"> <span class="color-red">*</span>Email </label>
+        <input type="text" class="auth-inputs" />
+      </div>
+
+      <div>
+        <label for="Password"> <span class="color-red">*</span>Password </label>
+        <input type="text" class="auth-inputs" />
+      </div>
+
+      <div>
+        <button class="bg-black text-white p-3 cursor-pointer w-full">
+          Log in
+        </button>
+        <p class="text-xs text-center mt-2 text-gray-800">
+          Don't have an account?
+          <span class="font-bold cursor-pointer">Register</span>
+        </p>
+      </div>
+    </form>
+  </div>
+</template>
