b4c0ea9cb7408041de7cfda39a8c9239b11396a4
Aleksa <aleksamlsvc12@gmail.com>
2025-09-26 15:11:56 +0200

    Views imported and tested

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,11 @@
 <script setup>
-import Header from "./components/Header.vue";
-import Main from "./components/Main.vue";
-import Footer from "./components/Footer.vue";
-
-import WelcomeSection from "./components/WelcomeSection.vue";
-
-import Register from "./components/Register.vue";
-import Login from "./components/Login.vue";
+import LandingPageView from './views/LandingPageView.vue';
+import RegisterPageView from './views/RegisterPageView.vue';
+import LoginPageView from './views/LoginPageView.vue';
 </script>
 
 <template>
   <div class="flex flex-col h-screen w-full">
-    <Header/>
-    <Main>
-      <Login/>
-    </Main>
-    <Footer/>
+    <LoginPageView/>
   </div>
 </template>
