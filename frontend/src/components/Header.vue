86645f3e4e49fa6d3e8ae5d617a4080f5e763d37
Aleksa <aleksamlsvc12@gmail.com>
2025-09-26 11:56:32 +0200

    Header component created

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="h-[10%] flex justify-between items-center pl-[10%] pr-[10%] font-mono">
+    <span class="text-2xl cursor-pointer">Typo</span>
+
+    <div class="flex gap-2">
+      <button class="border-1 p-1 pl-3 pr-3 cursor-pointer">Login</button>
+      <button class="border-1 p-1 pl-2 pr-2 cursor-pointer flex justify-center items-center">
+        <i class="pi pi-moon"></i>
+      </button>
+    </div>
+  </div>
+</template>
