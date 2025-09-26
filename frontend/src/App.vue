1e4d556a8d53f53d9ebc5173700fe757725bef14
Aleksa <aleksamlsvc12@gmail.com>
2025-09-26 14:08:50 +0200

    Welcome section component tested

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,15 @@
 import Header from "./components/Header.vue";
 import Main from "./components/Main.vue";
 import Footer from "./components/Footer.vue";
+import WelcomeSection from "./components/WelcomeSection.vue";
 </script>
 
 <template>
   <div class="flex flex-col h-screen w-full">
     <Header/>
-    <Main/>
+    <Main>
+      <WelcomeSection></WelcomeSection>
+    </Main>
     <Footer/>
   </div>
 </template>
