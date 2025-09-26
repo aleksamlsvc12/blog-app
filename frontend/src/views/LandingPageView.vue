3258f28dcf643b59270639687b15c4220b96348f
Aleksa <aleksamlsvc12@gmail.com>
2025-09-26 15:10:50 +0200

    Landing page view created

---
+++ frontend/src/views/LandingPageView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from "../components/Header.vue";
+import Main from "../components/Main.vue";
+import Footer from "../components/Footer.vue";
+import WelcomeSection from "../components/WelcomeSection.vue";
+</script>
+
+<template>
+  <Header />
+  <Main>
+    <WelcomeSection />
+  </Main>
+  <Footer />
+</template>
