97a2190f933211d99ba52d62dc8e87bc88ab3d51
Aleksa <aleksamlsvc12@gmail.com>
2025-09-26 14:52:07 +0200

    Auth components imported and tested

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,14 +2,18 @@
 import Header from "./components/Header.vue";
 import Main from "./components/Main.vue";
 import Footer from "./components/Footer.vue";
+
 import WelcomeSection from "./components/WelcomeSection.vue";
+
+import Register from "./components/Register.vue";
+import Login from "./components/Login.vue";
 </script>
 
 <template>
   <div class="flex flex-col h-screen w-full">
     <Header/>
     <Main>
-      <WelcomeSection></WelcomeSection>
+      <Login/>
     </Main>
     <Footer/>
   </div>
