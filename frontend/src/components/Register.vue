4a90f1b955c0efaaf9f58138a62dca88c87c3ace
Aleksa <aleksamlsvc12@gmail.com>
2025-09-26 14:44:26 +0200

    Register component created

---
+++ frontend/src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="h-full flex justify-center items-center font-mono">
+    <form class="flex flex-col justify-between w-1/4 h-[85%] p-10 bg-white">
+      <p class="text-lg font-bold text-center">Register</p>
+
+      <div>
+        <label for="Name"> <span class="color-red">*</span>Name </label>
+        <input type="text" class="auth-inputs" />
+      </div>
+
+      <div>
+        <label for="Surname"> <span class="color-red">*</span>Surname </label>
+        <input type="text" class="auth-inputs" />
+      </div>
+
+      <div>
+        <label for="Email"> <span class="color-red">*</span>Email </label>
+        <input type="text" class="auth-inputs" />
+      </div>
+
+      <div>
+        <label for="Password"> <span class="color-red">*</span>Password </label>
+        <input type="text" class="auth-inputs" />
+      </div>
+
+      <div>
+        <button class="bg-black text-white p-3 cursor-pointer w-full">
+          Register
+        </button>
+        <p class="text-xs text-center mt-2 text-gray-800">
+          Already have an account?
+          <span class="font-bold cursor-pointer">Log in</span>
+        </p>
+      </div>
+    </form>
+  </div>
+</template>
