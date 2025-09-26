3548c93807ac84e79d84766411879a287d1a00fd
Aleksa <aleksamlsvc12@gmail.com>
2025-09-26 14:08:24 +0200

    Welcome section component created

---
+++ frontend/src/components/WelcomeSection.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="w-1/2 h-full font-mono flex flex-col justify-center items-center"
+  >
+    
+    <div class="w-[70%] flex flex-col gap-5">
+      <p class="text-5xl font-bold">Welcome to Typo!</p>
+      <p>
+        Your personal blogging platform to share ideas, stories, and knowledge.
+        <span class="block mt-2">Write. Read. Connect with the community.</span>
+      </p>
+
+      <button class="self-start flex items-center border-1 p-3 cursor-pointer mt-5">
+        Get Started!
+        <i class="pi pi-unlock ml-1"></i>
+      </button>
+    </div>
+  </div>
+</template>
