5e2f14c1757f6abd910a0415bd1ae70161a99775
Aleksa <aleksamlsvc12@gmail.com>
2025-09-27 20:37:16 +0200

    Category component created

---
+++ frontend/src/components/Category.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="border-1 w-[220px] h-[250px] p-5 flex flex-col justify-between cursor-pointer hover:scale-[0.95] transition gap-4 bg-white">
+    <div class="bg-black text-white p-2">
+      <p class="text-sm font-bold text-center">Category Name</p>
+    </div>
+
+    <p class="text-xs text-justify">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dolorum aut
+      ullam iure non placeat a veniam nulla, sunt temporibus id vero fugit ipsa
+      assumenda facere quidem animi corrupti error.
+    </p>
+  </div>
+</template>
