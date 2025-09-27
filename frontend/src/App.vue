cd46ff4f89c7d82fde9b0881eb7990998c344ffd
Aleksa <aleksamlsvc12@gmail.com>
2025-09-27 20:39:25 +0200

    Categories page view tested

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,10 @@
+<script setup>
+import CategoriesView from "./views/CategoriesView.vue";
+</script>
+
 <template>
   <div class="flex flex-col h-screen w-full">
-    <router-view />
+    <!-- <router-view/> -->
+    <CategoriesView />
   </div>
 </template>
