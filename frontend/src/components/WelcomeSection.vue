88b9e49daa782c146d3dae0ce293fc7519163c94
Aleksa <aleksamlsvc12@gmail.com>
2025-09-27 18:00:43 +0200

    Landing page responsive design finished

--- frontend/src/components/WelcomeSection.vue
+++ frontend/src/components/WelcomeSection.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="w-1/2 h-full font-mono flex flex-col justify-center items-center">
-    <div class="w-[70%] flex flex-col gap-5">
-      <p class="text-5xl font-bold">Welcome to Typo!</p>
-      <p>
+  <div
+    class="w-full lg:w-1/2 h-full font-mono flex flex-col justify-center items-center"
+  >
+    <div
+      class="w-[90%] lg:w-[70%] flex flex-col gap-5 lg:items-start items-center"
+    >
+      <p class="text-5xl font-bold text-center lg:text-left">
+        Welcome to Typo!
+      </p>
+      <p class="text-center lg:text-left">
         Your personal blogging platform to share ideas, stories, and knowledge.
         <span class="block mt-2">Write. Read. Connect with the community.</span>
       </p>
