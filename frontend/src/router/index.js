66087d832ffcc072c9def9c89a26a47c1a1fa656
Aleksa <aleksamlsvc12@gmail.com>
2025-09-27 20:41:53 +0200

    Route for Categories page view created

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import LandingPageView from '../views/LandingPageView.vue';
 import RegisterPageView from '../views/RegisterPageView.vue';
 import LoginPageView from '../views/LoginPageView.vue';
+import CategoriesView from '../views/CategoriesView.vue';
 
 const routes = [
   {path: '/', name: 'Landing Page', component: LandingPageView},
   {path: '/login', name: 'Login', component: LoginPageView},
-  {path: '/register', name: 'Register', component: RegisterPageView}
+  {path: '/register', name: 'Register', component: RegisterPageView},
+  {path: '/categories', name: 'Categories', component: CategoriesView}
 ]
 
 const router = createRouter({
