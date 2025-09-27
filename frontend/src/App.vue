028830a862d354ba45ca94b8c1625e36e041b516
Aleksa <aleksamlsvc12@gmail.com>
2025-09-27 17:26:23 +0200

    Using router and showing router view in app component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,5 @@
-<script setup>
-import LandingPageView from './views/LandingPageView.vue';
-import RegisterPageView from './views/RegisterPageView.vue';
-import LoginPageView from './views/LoginPageView.vue';
-</script>
-
 <template>
   <div class="flex flex-col h-screen w-full">
-    <LoginPageView/>
+    <router-view />
   </div>
 </template>
