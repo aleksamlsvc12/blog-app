4924aa82227c7a22d7fb1c2cb6371918fef26791
Aleksa <aleksamlsvc12@gmail.com>
2025-09-27 20:37:39 +0200

    Categories section component created

---
+++ frontend/src/components/CategoriesSection.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Category from "./Category.vue";
+</script>
+
+<template>
+  <div class="p-10 h-full overflow-y-auto font-mono">
+    <p class="text-center mb-10 text-2xl font-bold border-1 p-4">Discover What Interests You</p>
+    <div class="grid lg:grid-cols-5 gap-y-10 justify-items-center ">
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+    </div>
+  </div>
+</template>
