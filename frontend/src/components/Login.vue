2dd677ef36101be6a7f14e940ba98a9b6cabff7c
Aleksa <aleksamlsvc12@gmail.com>
2025-09-29 18:53:18 +0200

    Communication between FE and BE established for login page

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,11 +1,67 @@
 <script setup>
-  import { ref } from 'vue';
-  const passwordInput = ref(null);
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const passwordInput = ref(null);
+
+const email = ref("");
+const password = ref("");
+const message = ref("");
+const success = ref(false);
+
+const router = useRouter();
+
+const loginUser = async () => {
+  try {
+    const res = await axios.post(
+      "http://localhost:8000/api/login.php",
+      {
+        email: email.value,
+        password: password.value,
+      },
+      { headers: { "Content-Type": "application/json" } }
+    );
+
+    console.log("✅", res.status, res.data);
+
+    if (res.data && res.data.ok === true) {
+      success.value = true;
+    } else {
+      success.value = false;
+    }
+
+    if (res.data && res.data.message) {
+      message.value = res.data.message;
+    } else {
+      message.value = JSON.stringify(res.data);
+    }
+
+    if (success.value) router.push("/categories");
+  } catch (err) {
+    if (err.response) {
+      console.log("❌", err.response.status, err.response.data);
+    } else {
+      console.log("❌ No response");
+    }
+
+    if (err.response && err.response.data && err.response.data.errors) {
+      const errorsObj = err.response.data.errors;
+      message.value = Object.values(errorsObj).join(" ");
+    } else if (err.response && err.response.data && err.response.data.message) {
+      message.value = err.response.data.message;
+    } else {
+      message.value = "Server error";
+    }
+
+    success.value = false;
+  }
+};
 
 function passVisible() {
   if (passwordInput.value.type == "text") {
     passwordInput.value.type = "password";
-  }else{
+  } else {
     passwordInput.value.type = "text";
   }
 }
@@ -14,19 +70,25 @@ function passVisible() {
 <template>
   <div class="h-full flex justify-center items-center font-mono">
     <form
+      @submit.prevent="loginUser"
       class="flex flex-col justify-between w-[80%] lg:w-1/4 h-[70%] p-10 bg-white"
     >
       <p class="text-lg font-bold text-center">Login</p>
 
       <div>
         <label for="Email">Email </label>
-        <input type="email" class="auth-inputs" />
+        <input v-model="email" type="email" class="auth-inputs" />
       </div>
 
       <div>
         <label for="Password">Password </label>
         <div class="relative">
-          <input ref="passwordInput" type="password" class="auth-inputs" />
+          <input
+            v-model="password"
+            ref="passwordInput"
+            type="password"
+            class="auth-inputs"
+          />
           <button
             @click.prevent="passVisible()"
             id="passwordButton"
@@ -38,17 +100,22 @@ function passVisible() {
       </div>
 
       <div>
-        <RouterLink to="/categories">
-          <button class="bg-black text-white p-3 cursor-pointer w-full">
+          <button
+            type="submit"
+            class="bg-black text-white p-3 cursor-pointer w-full"
+          >
             Log in
           </button>
-        </RouterLink>
 
         <p class="text-xs text-center mt-2 text-gray-800">
           Don't have an account?
           <RouterLink to="/register" class="font-bold cursor-pointer"
             >Register</RouterLink
           >
+        </p>
+
+        <p v-if="message" class="text-center text-xs text-red-500 mt-4">
+          {{ message }}
         </p>
       </div>
     </form>
