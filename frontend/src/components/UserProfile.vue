3abe23ff98d98b993b3411ea1b005e2980912753
Aleksa <aleksamlsvc12@gmail.com>
2025-10-02 09:27:42 +0200

    Created  initial User profile component

---
+++ frontend/src/components/UserProfile.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { useAuthStore } from "../stores/auth";
+const auth = useAuthStore();
+</script>
+
+<template>
+  <RouterLink to="/">
+    <button @click="auth.logout()" class="border-1 cursor-pointer">Logout</button>
+  </RouterLink>
+</template>
