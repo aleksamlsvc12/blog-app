0b73fb339ff6bfe2961a808a342413e57886950a
Aleksa <aleksamlsvc12@gmail.com>
2025-10-02 11:05:54 +0200

    Category View takes the categories data, passes it to Category Section

--- frontend/src/views/CategoriesView.vue
+++ frontend/src/views/CategoriesView.vue
@@ -1,4 +1,14 @@
 <script setup>
+import { ref, onMounted } from "vue";
+import axios from 'axios';
+
+const categories = ref([]);
+
+onMounted(async () => {
+  const res = await axios.get('http://localhost:8000/api/categories.php');
+  categories.value = res.data;
+});
+
 import Header from "../components/Header.vue";
 import Main from "../components/Main.vue";
 import Footer from "../components/Footer.vue";
@@ -8,7 +18,7 @@ import CategoriesSection from "../components/CategoriesSection.vue";
 <template>
   <Header />
   <Main>
-    <CategoriesSection/>
+    <CategoriesSection :categories="categories"/>
   </Main>
   <Footer />
 </template>
