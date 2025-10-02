c1b8f12928a53f9be6808e6c720a4f7173287d6b
Aleksa <aleksamlsvc12@gmail.com>
2025-10-02 09:28:09 +0200

    User profile view created

---
+++ frontend/src/views/UserProfileView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from "../components/Header.vue";
+import Main from "../components/Main.vue";
+import Footer from "../components/Footer.vue";
+import UserProfile from "../components/UserProfile.vue";
+</script>
+
+<template>
+  <Header/>
+  <Main>
+    <UserProfile/>
+  </Main>
+  <Footer/>
+</template>
