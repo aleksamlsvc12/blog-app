9d286075b3601d1f63c8b6375061d70b0421fc0d
Aleksa <aleksamlsvc12@gmail.com>
2025-10-02 11:06:51 +0200

    Category card gets the data and displays it

--- frontend/src/components/Category.vue
+++ frontend/src/components/Category.vue
@@ -1,13 +1,24 @@
+<script setup>
+defineProps({
+  category: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
 <template>
-  <div class="border-1 w-[220px] h-[250px] p-5 flex flex-col justify-between cursor-pointer hover:scale-[0.95] transition gap-4 bg-white">
+  <div
+    class="border-1 w-[200px] h-[210px] p-5 flex flex-col justify-between cursor-pointer hover:scale-[0.95] transition gap-4 bg-white"
+  >
     <div class="bg-black text-white p-2">
-      <p class="text-sm font-bold text-center">Category Name</p>
+      <p class="text-sm font-bold text-center">{{ category.name }}</p>
     </div>
 
-    <p class="text-xs text-justify">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dolorum aut
-      ullam iure non placeat a veniam nulla, sunt temporibus id vero fugit ipsa
-      assumenda facere quidem animi corrupti error.
-    </p>
+    <div class="border h-[80%] flex justify-center items-center">
+      <p class="text-center text-xs">
+        {{ category.description }}
+      </p>
+    </div>
   </div>
 </template>
