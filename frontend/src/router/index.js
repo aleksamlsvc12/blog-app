9b97f9b8f37e0c0bc83314a4efaf8480d122517c
Aleksa <aleksamlsvc12@gmail.com>
2025-10-02 09:28:46 +0200

    Route to UserProfileView created in the router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,12 +4,14 @@ import LandingPageView from '../views/LandingPageView.vue';
 import RegisterPageView from '../views/RegisterPageView.vue';
 import LoginPageView from '../views/LoginPageView.vue';
 import CategoriesView from '../views/CategoriesView.vue';
+import UserProfileView from '../views/UserProfileView.vue';
 
 const routes = [
   {path: '/', name: 'Landing Page', component: LandingPageView},
   {path: '/login', name: 'Login', component: LoginPageView},
   {path: '/register', name: 'Register', component: RegisterPageView},
-  {path: '/categories', name: 'Categories', component: CategoriesView}
+  {path: '/categories', name: 'Categories', component: CategoriesView},
+  {path: '/profile', name: 'User Profile', component: UserProfileView}
 ]
 
 const router = createRouter({
