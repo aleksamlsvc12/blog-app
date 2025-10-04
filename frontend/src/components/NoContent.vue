6fc4ff1f6ed9a94e05c4240889d58acae9196bed
Aleksa <aleksamlsvc12@gmail.com>
2025-10-04 14:18:34 +0200

    No content component created

---
+++ frontend/src/components/NoContent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full h-full flex flex-col justify-center items-center p-10">
+    <div>
+      <p class="text-xl font-bold">
+        Oops! Looks like there are no blogs on this topic.
+      </p>
+      <RouterLink to="/post">
+        <p class="text-sm cursor-pointer">Create one now!</p>
+      </RouterLink>
+    </div>
+  </div>
+</template>
