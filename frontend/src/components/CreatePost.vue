efa330f14c6bb2ea44c41cfdf2fd5d932fda31fd
Aleksa <aleksamlsvc12@gmail.com>
2025-10-04 12:11:29 +0200

    CreatePost component created

---
+++ frontend/src/components/CreatePost.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="w-full h-full p-8 flex justify-center items-center font-mono">
+    <form
+      class="bg-white p-8 lg:w-1/4 w-full h-full flex flex-col justify-around items-center"
+    >
+      <p class="text-lg font-bold">Create post</p>
+
+      <div class="flex flex-col justify-center items-center gap-4 h-[95%] w-full">
+        <div class="w-full">
+          <label for="Title" class="text-sm">Title</label>
+          <input type="text" class="auth-inputs" />
+        </div>
+
+        <div class="w-full">
+          <label for="Description" class="text-sm block">Description</label>
+          <textarea name="post-description" id="post-description" class="border w-full h-[200px] resize-none p-2 text-xs"></textarea>
+        </div>
+
+        <div class="w-full">
+          <label for="Thumbnail" class="text-sm">Thumbnail</label>
+          <input
+            type="file"
+            class="file:py-1 file:px-3 file:border-1 file:cursor-pointer block text-xs border p-2 text-center w-full"
+          />
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
