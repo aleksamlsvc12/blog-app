97ee4ff88acd35371427492338dbe4d65cebf7be
Aleksa <aleksamlsvc12@gmail.com>
2025-10-04 12:11:42 +0200

    Create Post View created

---
+++ frontend/src/views/CreatePostView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from '../components/Header.vue';
+import Main from '../components/Main.vue';
+import Footer from '../components/Footer.vue';
+import CreatePost from '../components/CreatePost.vue';
+</script>
+
+<template>
+  <Header/>
+  <Main>
+    <CreatePost/>
+  </Main>
+  <Footer/>
+</template>
