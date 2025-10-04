c83d1b06b8174fdf4c66ad9a4e38bc76d9c4c68a
Aleksa <aleksamlsvc12@gmail.com>
2025-10-04 16:54:23 +0200

    Get categories backend updated

--- backend/api/getCategories.php
+++ backend/api/getCategories.php
@@ -5,6 +5,11 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json; charset=utf-8");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  http_response_code(204);
+  exit;
+}
+
 require_once '../data/db.php';
 
 $sql = "SELECT id, name FROM categories ORDER BY name ASC";
@@ -16,9 +21,15 @@ if ($result && mysqli_num_rows($result) > 0) {
   while ($row = mysqli_fetch_assoc($result)) {
     $categories[] = $row;
   }
-}
 
-echo json_encode([
-  "status" => "success",
-  "categories" => $categories
-]);
+  echo json_encode([
+    "status" => "success",
+    "categories" => $categories
+  ]);
+} else {
+  echo json_encode([
+    "status" => "empty",
+    "categories" => []
+  ]);
+}
+?>
