45856ed33ea0082158d9c64a50c7380cc7b5032d
Aleksa <aleksamlsvc12@gmail.com>
2025-10-04 16:11:53 +0200

    Routes updated - added routes for no content and specific category view page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,20 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
-import LandingPageView from '../views/LandingPageView.vue';
-import RegisterPageView from '../views/RegisterPageView.vue';
-import LoginPageView from '../views/LoginPageView.vue';
-import CategoriesView from '../views/CategoriesView.vue';
-import UserProfileView from '../views/UserProfileView.vue';
-import CreatePostView from '../views/CreatePostView.vue';
+import LandingPageView from "../views/LandingPageView.vue";
+import RegisterPageView from "../views/RegisterPageView.vue";
+import LoginPageView from "../views/LoginPageView.vue";
+import CategoriesView from "../views/CategoriesView.vue";
+import UserProfileView from "../views/UserProfileView.vue";
+import CreatePostView from "../views/CreatePostView.vue";
+import NoContentView from "../views/NoContentView.vue";
+import SpecificCategoryView from "../views/SpecificCategoryView.vue";
 
 const routes = [
-  {path: '/', name: 'Landing Page', component: LandingPageView},
-  {path: '/login', name: 'Login', component: LoginPageView},
-  {path: '/register', name: 'Register', component: RegisterPageView},
-  {path: '/categories', name: 'Categories', component: CategoriesView},
-  {path: '/profile', name: 'User Profile', component: UserProfileView},
-  {path: '/post', name: 'Create Post', component: CreatePostView}
-]
+  { path: "/", name: "Landing Page", component: LandingPageView },
+  { path: "/login", name: "Login", component: LoginPageView },
+  { path: "/register", name: "Register", component: RegisterPageView },
+  { path: "/categories", name: "Categories", component: CategoriesView },
+  { path: "/profile", name: "User Profile", component: UserProfileView },
+  { path: "/post", name: "Create Post", component: CreatePostView },
+  { path: "/no-content", name: "No content", component: NoContentView },
+  { path: "/category/:name", name: "Category", component: SpecificCategoryView, },
+];
 
 const router = createRouter({
   history: createWebHistory(),
