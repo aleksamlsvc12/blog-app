67f8ba6871d18ef6e2be0e1567296c41fe6d9150
Aleksa <aleksamlsvc12@gmail.com>
2025-10-06 13:04:39 +0200

    Edit profile view page created

---
+++ frontend/src/views/EditProfileView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from '../components/Header.vue';
+import Main from '../components/Main.vue';
+import Footer from '../components/Footer.vue';
+import EditProfile from '../components/EditProfile.vue';
+</script>
+
+<template>
+  <Header/>
+  <Main>
+    <EditProfile/>
+  </Main>
+  <Footer/>
+</template>
