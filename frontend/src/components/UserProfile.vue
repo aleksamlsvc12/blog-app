f651d1bd5d2cbf715b93f0c5accc3c19dd8be6d2
Aleksa <aleksamlsvc12@gmail.com>
2025-10-06 12:01:20 +0200

    User profile component - responsive design improved

--- frontend/src/components/UserProfile.vue
+++ frontend/src/components/UserProfile.vue
@@ -7,12 +7,6 @@ const auth = useAuthStore();
 const userName = ref("");
 const created_at = ref("");
 
-let date = ref("");
-let year = ref("");
-let month = ref("");
-let day = ref("");
-
-
 onMounted(async () => {
   const res = await axios.get("http://localhost:8000/api/getUser.php", {
     params: { id: auth.user.id },
@@ -20,10 +14,10 @@ onMounted(async () => {
   if (res.data.success) {
     userName.value = `${res.data.name} ${res.data.surname}`;
 
-    date = new Date(res.data.created_at);
-    year = date.getFullYear();
-    month = String(date.getMonth() + 1);
-    day = String(date.getDate());
+    const date = new Date(res.data.created_at);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1);
+    const day = String(date.getDate());
 
     created_at.value = `${year}-${month}-${day}`;
   }
@@ -31,16 +25,18 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="w-full h-full p-20 flex justify-center items-center">
-    <div class="flex w-full justify-between h-[70%] gap-10">
-      <div class="w-1/4 h-full border flex justify-center items-center p-5">
+  <div class="w-full h-full lg:p-20 p-10 flex lg:flex-row flex-col justify-center items-center">
+
+    <div class="flex lg:flex-row flex-col w-full justify-between lg:h-[70%] h-full gap-10">
+      <div class="lg:w-1/4 w-full h-full border flex justify-center items-center p-5">
         <div
           class="w-full h-full bg-gray-400 flex justify-center items-center"
         >
           <i class="pi pi-user text-9xl"></i>
         </div>
       </div>
-      <div class="w-[75%] h-full border p-10 flex flex-col">
+
+      <div class="lg:w-[75%] overflow-y-auto w-full h-full border p-10 flex flex-col">
         <div>
           <p class="text-4xl font-bold">{{ userName }}</p>
         </div>
@@ -50,12 +46,14 @@ onMounted(async () => {
           <hr />
         </div>
 
-        <div class="w-full h-[20%] p-2 flex justify-end">
-          <button class="border p-1 pl-2 pr-2 cursor-pointer">
+        <div class="w-full lg:h-[20%] p-2 flex justify-end">
+          <button class="border p-1 pl-2 pr-2 cursor-pointer flex justify-center items-center text-sm">
             Edit Profile
           </button>
         </div>
       </div>
+
     </div>
+
   </div>
 </template>
