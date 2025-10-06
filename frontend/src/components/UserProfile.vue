bc9b6ce9630ed4ea84f51677eed246f4a69659d0
Aleksa <aleksamlsvc12@gmail.com>
2025-10-06 11:42:45 +0200

    FE connected to BE - user profile

--- frontend/src/components/UserProfile.vue
+++ frontend/src/components/UserProfile.vue
@@ -1,40 +1,59 @@
 <script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
 import { useAuthStore } from "../stores/auth";
+
 const auth = useAuthStore();
+const userName = ref("");
+const created_at = ref("");
+
+let date = ref("");
+let year = ref("");
+let month = ref("");
+let day = ref("");
+
+
+onMounted(async () => {
+  const res = await axios.get("http://localhost:8000/api/getUser.php", {
+    params: { id: auth.user.id },
+  });
+  if (res.data.success) {
+    userName.value = `${res.data.name} ${res.data.surname}`;
+
+    date = new Date(res.data.created_at);
+    year = date.getFullYear();
+    month = String(date.getMonth() + 1);
+    day = String(date.getDate());
+
+    created_at.value = `${year}-${month}-${day}`;
+  }
+});
 </script>
 
 <template>
   <div class="w-full h-full p-20 flex justify-center items-center">
     <div class="flex w-full justify-between h-[70%] gap-10">
       <div class="w-1/4 h-full border flex justify-center items-center p-5">
-        <div class="w-full h-full bg-gray-400"></div>
+        <div
+          class="w-full h-full bg-gray-400 flex justify-center items-center"
+        >
+          <i class="pi pi-user text-9xl"></i>
+        </div>
       </div>
-      <div class="w-[75%] h-full border p-10 flex flex-col justify-between">
+      <div class="w-[75%] h-full border p-10 flex flex-col">
         <div>
-          <p class="text-4xl font-bold">Aleksa Milosevic</p>
-          <p class="text-xl">Frontend developer</p>
+          <p class="text-4xl font-bold">{{ userName }}</p>
         </div>
 
         <div>
-          <p class="mt-2 text-sm">Since: 2025</p>
+          <p class="text-sm">Since: {{ created_at }}</p>
           <hr />
         </div>
 
-        <p class="text-xs mt-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero magnam
-          suscipit, esse iste natus architecto explicabo beatae cumque? Quae
-          atque sint repudiandae non tenetur tempora ratione totam dicta ab?
-          Quisquam? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex
-          magnam debitis voluptatibus dolore nam perspiciatis fugit tempora eum
-          consequuntur delectus, autem quisquam eos molestiae odio soluta,
-          veniam quod maiores quo? Lorem, ipsum dolor sit amet consectetur
-          adipisicing elit. Dignissimos, placeat mollitia autem, necessitatibus
-          consectetur commodi magni modi vitae voluptas veritatis architecto
-          optio. Perspiciatis, at adipisci. Doloremque accusantium quo eum
-          molestias.
-        </p>
         <div class="w-full h-[20%] p-2 flex justify-end">
-          <button class="border p-1 pl-2 pr-2 cursor-pointer">Edit Profile</button>
+          <button class="border p-1 pl-2 pr-2 cursor-pointer">
+            Edit Profile
+          </button>
         </div>
       </div>
     </div>
