e0a268339efe524e6242bf86ed15e912a028ef71
Aleksa <aleksamlsvc12@gmail.com>
2025-10-06 13:12:45 +0200

    Get user php file updated

--- backend/api/getUser.php
+++ backend/api/getUser.php
@@ -5,6 +5,11 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json; charset=utf-8");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  http_response_code(204);
+  exit;
+}
+
 require_once '../data/db.php';
 
 $id = $_GET['id'] ?? 0;
