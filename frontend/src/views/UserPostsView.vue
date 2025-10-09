9f4389ab0509585f10fc89f9e8fbc73a995cb8b1
Aleksa <aleksamlsvc12@gmail.com>
2025-10-09 16:51:51 +0200

    User Posts View page created

---
+++ frontend/src/views/UserPostsView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from '../components/Header.vue';
+import Main from '../components/Main.vue';
+import Footer from '../components/Footer.vue';
+import UsersPosts from '../components/UsersPosts.vue';
+</script>
+
+<template>
+  <Header/>
+  <Main>
+    <UsersPosts/>
+  </Main>
+  <Footer/>
+</template>
