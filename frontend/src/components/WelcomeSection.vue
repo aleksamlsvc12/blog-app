5ccdb3bb43c413e43720e6ba2fb6a178461a4446
Aleksa <aleksamlsvc12@gmail.com>
2025-10-11 13:50:10 +0200

    Welcome section updated

--- frontend/src/components/WelcomeSection.vue
+++ frontend/src/components/WelcomeSection.vue
@@ -6,12 +6,12 @@ const auth = useAuthStore();
 
 <template>
   <div
-    class="w-full lg:w-1/2 h-full font-mono flex flex-col justify-center items-center"
+    class="w-full lg:w-1/2 h-full font-mono flex flex-col justify-center items-center text-gray-100"
   >
     <div
       class="w-[90%] lg:w-[70%] flex flex-col gap-5 lg:items-start items-center"
     >
-      <p class="text-5xl font-bold text-center lg:text-left">
+      <p class="text-5xl font-bold text-center lg:text-left text-white">
         Welcome to Typo!
       </p>
       <p class="text-center lg:text-left">
@@ -21,10 +21,10 @@ const auth = useAuthStore();
 
       <RouterLink to="/register" v-if="!auth.isLoggedIn">
         <button
-          class="self-start flex items-center border-1 p-3 cursor-pointer mt-5"
+          class="font-bold flex items-center bg-emerald-800 text-gray-100 py-2 px-4 rounded-md cursor-pointer mt-5 transition-all duration-300 hover:bg-emerald-700 hover:shadow-[0_0_18px_rgba(16,185,129,0.7)] active:scale-95"
         >
           Get Started!
-          <i class="pi pi-unlock ml-1"></i>
+          <i class="pi pi-unlock ml-2"></i>
         </button>
       </RouterLink>
     </div>
