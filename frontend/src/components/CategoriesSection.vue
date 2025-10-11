93a77039af619eb7726c32a219caa0d77e35059f
Aleksa <aleksamlsvc12@gmail.com>
2025-10-11 14:23:21 +0200

    Categories section updated

--- frontend/src/components/CategoriesSection.vue
+++ frontend/src/components/CategoriesSection.vue
@@ -12,7 +12,7 @@ import Category from "./Category.vue";
 
 <template>
   <div class="p-10 h-full overflow-y-auto font-mono">
-    <p class="mt-5 mb-10 text-2xl font-bold border p-4 text-center lg:text-left">Discover What Interests You</p>
+    <p class="mb-10 text-2xl font-bold text-white p-4 text-center lg:text-left">Discover What Interests You</p>
     <div class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-y-10 justify-items-center ">
       <Category 
         v-for="category in categories"
