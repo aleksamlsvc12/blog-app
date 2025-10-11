5742849249099f081c32052fc40704cc85c045cf
Aleksa <aleksamlsvc12@gmail.com>
2025-10-11 13:49:14 +0200

    Footer updated

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-[5%] p-3 font-mono flex justify-center items-center">
+  <div class="h-[5%] p-3 font-mono flex justify-center items-center bg-[#0F172A] text-gray-200">
     <p class="text-xs">&copy; 2025 Typo. All rights reserved.</p>
   </div>
 </template>
