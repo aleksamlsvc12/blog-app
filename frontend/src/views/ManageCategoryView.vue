08692b72ed01d3eebfe481c31f1e3ce1f1034858
Aleksa <aleksamlsvc12@gmail.com>
2025-10-15 13:56:40 +0200

    Add ManageCategoryView component to integrate category management UI

---
+++ frontend/src/views/ManageCategoryView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from "../components/Header.vue";
+import Main from "../components/Main.vue";
+import Footer from "../components/Footer.vue";
+import ManageCategory from "../components/ManageCategory.vue";
+</script>
+
+<template>
+  <Header />
+  <Main>
+    <ManageCategory />
+  </Main>
+  <Footer />
+</template>
